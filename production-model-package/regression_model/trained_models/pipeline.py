eebd3314c2fa8b00d54adbfbc8350227ea979867
Aamir <58370693+AIsquare@users.noreply.github.com>
2021-11-11 19:40:15 +0530

    Add files via upload

---
+++ production-model-package/regression_model/trained_models/pipeline.py
@@ -0,0 +1,19 @@
+# from Scikit-learn
+from sklearn.linear_model import Lasso
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+
+
+
+from regression_model.config.core import config
+from regression_model.processing import features as pp
+
+from feature_engine.selection import DropFeatures
+
+dur_pipe = Pipeline([('drop_features', DropFeatures(features_to_drop=[config.model_config.ref_var])),
+
+                     ('scaler', MinMaxScaler()),
+                     ('Lasso', Lasso(alpha=config.model_config.alpha,
+                random_state=config.model_config.random_state))])
